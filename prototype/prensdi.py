fc05c1d06f2d766e19fd0caa4d6135aae6cabcea
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-10-02 16:02:13 -0400

    Import policy and TPVP support for pre-NSDI rules

--- prototype/prensdi.py
+++ prototype/prensdi.py
@@ -331,7 +331,13 @@ class TPG(graph.Graph):
                                 self.bgp_name(neighbor.iface, "I"))):
                         self.add_edge(self.bgp_name(vlan, "O"),
                                 self.bgp_name(neighbor.iface, "I"),
-                                combine=False, color="orange")
+                                combine=False, color="orange", 
+                                label=neighbor.import_policy)
+                        if (neighbor.import_policy is not None):
+                            print("%s->%s %s" % (self.bgp_name(vlan, "O"), 
+                                self.bgp_name(neighbor.iface, "I"), 
+                                neighbor.import_policy))
+
 
     def add_src_to_ospf_edges(self, router):
         for vlan in router.vlans.values():
@@ -392,5 +398,109 @@ class TPG(graph.Graph):
         src = str(edge[0]).split(':')[0]
         dst = str(edge[1]).split(':')[0]
         return ([src,dst] in failset or [dst,src] in failset)
+
+    def tpvp(self):
+        # Line 2
+        path = {}
+        sign = {}
+        bestpath = {}
+        bestsign = {}
+        for u in self._graph.nodes():
+            path[u] = {}
+            sign[u] = {}
+            bestpath[u] = None
+            bestsign[u] = None
+            for v in self._graph.out_neighbors(u):
+                path[u][v] = None
+                sign[u][v] = None
+
+        # Line 3
+        dst = self.get_vertex(self._t)
+        bestpath[dst] = [dst]
+        bestsign[dst] = {'lp':0,'len':0,'cost':0}
+        
+        change = True
+        i = 0
+
+        # Line 4
+        while change:
+            i += 1
+
+            print('ROUND %d' % i)
+            for v in sorted(bestpath.keys()):
+                print('\t%s %s' % (bestpath[v], bestsign[v]))
+
+            change = False
+
+            # Line 5
+            for u in self._graph.nodes():
+
+                if (u == dst): 
+                    continue
+
+                # Line 6
+                for e in self._graph.out_edges(u):
+                    v = e[1]
+
+                    if (bestpath[v] is not None and u not in bestpath[v]):
+
+                        # Line 7
+                        path[u][v] = [u] + bestpath[v]
+
+                        # Line 8
+                        L = {}
+                        if "label" in e.attr and e.attr["label"] != '':
+                            L = eval(e.attr["label"])
+                        sign[u][v] = self.sign_combine(L, bestsign[v])
+
+                # Line 9
+                newbestpath, newbestsign = self.path_rank(u, path[u], sign[u],
+                        bestpath[u], bestsign[u])
+
+                # Line 10
+                if newbestpath != bestpath[u] or newbestsign != bestsign[u]:
+
+#                    print("CHANGE: %s" % u)
+                    bestpath[u] = newbestpath
+                    bestsign[u] = newbestsign
+
+                    # Line 11
+                    change = True
+
+        src = self.get_vertex(self._s)
+#        print(bestsign)
+        return (bestpath[src], bestsign[src])
+
+    def sign_combine(self, label, sign):
+        newsign = sign.copy()
+        for k,v in label.items():
+            if k == 'cost' or k == 'len':
+                newsign[k] = v + (newsign[k] if k in newsign else 0)
+            elif k == 'lp':
+                newsign[k] = v
+
+        return newsign
+
+    def path_rank(self, u, paths, signs, bestpath, bestsign):
+        for v,sign in signs.items():
+            if (sign is None):
+                continue
+
+            if (bestsign is None):
+                bestsign = sign
+                bestpath = paths[v]
+
+            if ("OSPF" in str(u)):
+                if (sign['cost'] < bestsign['cost']):
+                    bestsign = sign
+                    bestpath = paths[v]
+            elif ("BGP" in str(u)):
+                if (sign['lp'] > bestsign['lp'] 
+                        or (sign['lp'] == bestsign['lp'] 
+                            and sign['len'] < bestsign['len'])):
+                    bestsign = sign
+                    bestpath = paths[v]
+
+        return bestpath, bestsign
 
 
